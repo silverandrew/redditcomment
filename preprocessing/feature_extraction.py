5ec45a8390731eedb90b98cdbfd3d5a2839f39d8
Andrew Silver <aesilver@zoho.com>
2015-10-26 21:25:34 -0400

    added code for extracting features and saving .csv file for analysis

--- preprocessing/feature_extraction.py
+++ preprocessing/feature_extraction.py
@@ -2,18 +2,15 @@ import io
 import os
 import json
 import csv
+import string 
 
 #open json dataset and create a csv file with features and labels
 
-    
-
-#spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-#spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-
+#input file
 s = os.path.abspath("../data/new_data2.json")
 #print s
 
-#TODO: implement http://www.nltk.org/book/ch05.html
+#TODO: implement using http://www.nltk.org/book/ch05.html
 
 with open(s)as data_file:
   daa=json.load(data_file)
@@ -26,25 +23,24 @@ with open('reddit_no_labels.csv', 'wb') as csvfile:
 
     for line in daa:
       body=line['body']
+
+#slow, need to do because text is unicode
+#get rid of punctuation
+      exclude = set(string.punctuation)
+      body = ''.join(ch for ch in body if ch not in exclude)
     
       #count number of words
-    
       words = body.split(" ")
       
       #number of words
       #print "Number of words:"
       number_of_words = len(words)
     
-    #  print words[0] 
       #find # of characters in words
-    
-    #TODO: ignore punctuation
-      
       sum_of_word_lengths = 0
       for word in words:
         num_of_chars = sum(c != ' ' for c in word)
         sum_of_word_lengths = sum_of_word_lengths + num_of_chars
-    #    print(sum)
         # of chars divided by total # of words
     
       avg_word_length = sum_of_word_lengths / len(words)
@@ -60,6 +56,3 @@ with open('reddit_no_labels.csv', 'wb') as csvfile:
     #column 0 
       ups = line['ups']
       spamwriter.writerow([ups, author, subreddit, number_of_words, avg_word_length])
-
-#  if len(list) < 100:
-#    print line['author'],line['subreddit'],line['ups'] 
